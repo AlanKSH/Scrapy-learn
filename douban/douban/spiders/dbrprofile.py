5f50853320ab283910cb2e408b71d422b1b34596
Yaocheng Wang <yocson@outlook.com>
2016-01-14 14:24:39 +0800

    Delete cookies

--- douban/douban/spiders/dbrprofile.py
+++ douban/douban/spiders/dbrprofile.py
@@ -10,10 +10,9 @@ class DbprofileSpider(scrapy.spiders.Spider):
         "http://www.douban.com/people/48335885/",
     ]
 
-    def start_requests(self):  
-        for url in self.start_urls:          
-            yield Request(url, cookies={'_pk_id.100001.3ac3':'4ff929e3b35ff736.1452662726.3.1452746854.1452667708.', 
-                                        '__utmb':'81379588.1.10.1452746854'})  
+    #def start_requests(self):  
+    #    for url in self.start_urls:          
+    #        yield Request(url, cookies={''})  
 
     def parse(self, response):
         #filename = response.url.split("/")[-2]
